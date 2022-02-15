5de38223e374cc844e5a8886769c87c22ce26021
Abid Adhikari <abidadhikari@gmail.com>
2022-02-15 21:25:07 +0545

    preloader added and anish date updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react"
 import  ReactDOM  from "react-dom"
 import App from "./components/App"
-
+window.addEventListener("load",()=>{
+    document.body.style.overflowY="scroll";
+    document.getElementById("preloader").style.display="none";
+})
 ReactDOM.render(<App />, document.getElementById("root"))
